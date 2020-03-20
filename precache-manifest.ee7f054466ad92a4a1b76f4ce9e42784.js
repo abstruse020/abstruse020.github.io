f58bfeebdea3331e5a48a84de5a11b59e925017e
harsh <bokharsh@gmail.com>
2020-03-20 22:58:51 +0530

    Updates

---
+++ precache-manifest.ee7f054466ad92a4a1b76f4ce9e42784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c117a930bb4c7a7f9d4fa5a6044c1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc04d520cbcbb27399b9",
+    "url": "/static/css/main.ff618315.chunk.css"
+  },
+  {
+    "revision": "3e24cdcdff7b99af1049",
+    "url": "/static/js/2.53ac971d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.53ac971d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc04d520cbcbb27399b9",
+    "url": "/static/js/main.03bae739.chunk.js"
+  },
+  {
+    "revision": "36180433ff2c3de56d8c",
+    "url": "/static/js/runtime-main.50a66773.js"
+  },
+  {
+    "revision": "494f673ff11b5546e81d4757be629602",
+    "url": "/static/media/profile.494f673f.jpg"
+  }
+]);
